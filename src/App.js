dcdd5c6a0c375c292d850af8172159a08802bee1
Biswajit Biswas <jfbiswajit@gmail.com>
2022-06-04 16:56:06 +0600

    Add: EPS-18

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import { useState } from 'react';
 import FeedbackList from './components/FeedbackList.jsx';
+import FeedbackStats from './components/FeedbackStats.jsx';
 import Header from './components/Header.jsx';
 import feedbackData from './data/FeedbackData';
 
 function App() {
   const [feedback, setFeedback] = useState(feedbackData);
-  
+
   const deleteFeedback = (id) => {
-      if (window.confirm('Are you sure you want to delete?')) {
-        setFeedback(feedback.filter(item => item.id !== id));
-      }
-  }
-  
+    if (window.confirm('Are you sure you want to delete?')) {
+      setFeedback(feedback.filter((item) => item.id !== id));
+    }
+  };
+
   return (
     <div className='container'>
       <Header />
+      <FeedbackStats feedback={feedback} />
       <FeedbackList handleDelete={deleteFeedback} feedback={feedback} />
     </div>
   );
