e47b12256ec7ef4f1bb24dd97355902ce2109bb5
Biswajit Biswas <jfbiswajit@gmail.com>
2022-06-06 09:59:09 +0600

    Add: EPS-24

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,16 +1,28 @@
+import { motion, AnimatePresence } from 'framer-motion';
 import FeedbackItem from './FeedbackItem';
 
 function FeedbackList({ feedback, handleDelete }) {
   let content = <p>No feedback yet.</p>;
 
   if (feedback.length) {
-    content = feedback.map((feedbackItem) => (
-      <FeedbackItem
-        key={feedbackItem.id}
-        item={feedbackItem}
-        handleDelete={handleDelete}
-      />
-    ));
+    content = (
+      <AnimatePresence>
+        {feedback.map((feedbackItem) => (
+          <motion.div
+            key={feedbackItem.id}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+          >
+            <FeedbackItem
+              key={feedbackItem.id}
+              item={feedbackItem}
+              handleDelete={handleDelete}
+            />
+          </motion.div>
+        ))}
+      </AnimatePresence>
+    );
   }
 
   return <div className='feedback-list'> {content} </div>;
